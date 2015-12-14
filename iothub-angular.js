e68f7e186a70b194d67293cc44f500b164ba0253
Przemysław Płyś <przemyslaw.plys@gmail.com>
2015-12-14 16:45:28 +0100

    Created Thing resource, and added small fix to devDep

--- iothub-angular.js
+++ iothub-angular.js
@@ -1,4 +1,13 @@
-angular.module('iothub',['ngResource'])
-.consts('iothubConfig', {
-'host':'https://api.iothub.pl/'
-});
+angular.module('iothub', ['ngResource'])
+    .constant('iothubConfig', {
+        'host': 'https://api.iothub.pl/'
+    })
+    .config(['$resourceProvider', function ($resourceProvider) {
+        $resourceProvider.defaults.stripTrailingSlashes = false;
+    }])
+    .factory('ThingService'
+{
+
+}
+)
+;
